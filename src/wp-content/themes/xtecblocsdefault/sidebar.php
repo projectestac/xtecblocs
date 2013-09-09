4f2474c9a0ad1d79e6eb78280e3ace63b655c729
Jaume Miro <jmiro227@xtec.cat>
2013-09-09 13:17:40 +0200

    Canvi temporal mentre no hi ha espai de cursos

--- src/wp-content/themes/xtecblocsdefault/sidebar.php
+++ src/wp-content/themes/xtecblocsdefault/sidebar.php
@@ -6,11 +6,14 @@
 			<span class="blauleftcorner">&nbsp;</span>
 			<p class="creabloc"><a href="index.php?a=new">Crea el teu bloc<span></span></a></p>
 		</div>
+
 		<div id="cel">
 			<span class="celrightcorner">&nbsp;</span>
 			<span class="celleftcorner">&nbsp;</span>
-			<p class="espaicursos"><a id="espaicursoslink" target="_blank" href="http://hipolit2.xtec.cat/blocs">Espai de cursos<span id="espaicursos"></span></a></p>		
+			<!-- <p class="espaicursos"><a id="espaicursoslink" target="_blank" href="http://hipolit2.xtec.cat/blocs">Espai de cursos<span id="espaicursos"></span></a></p>-->
+			<p class="espaicursos"><a id="espaicursoslink" target="_blank" href="espai-de-cursos">Espai de cursos<span id="espaicursos"></span></a></p>		
 		</div>
+
 		<script>
 		 changeImage();
 		</script>
@@ -26,7 +29,7 @@
 			<?php $numberBlogs=getBlogsNumber();?>
 			<p>En aquests moments hi ha <?php echo $numberBlogs['blogs']; ?> blocs creats dels quals un <?php echo round($numberBlogs['blogsPrivate']*100/$numberBlogs['blogs'],2);?> % són privats.</p>
 			<h3>Avís legal</h3>
-			<p>XTECBlocs ha estat desenvolupat amb <a href="http://mu.wordpress.org/" target="_blank">WordPress MU</a>. Articles (<a href="feed">RSS</a>) i Comentaris (<a href="comments/feed">RSS</a>).</p>
+			<p>XTECBlocs ha estat desenvolupat amb <a href="http://wordpress.org/" target="_blank">WordPress</a>. Articles (<a href="feed">RSS</a>) i Comentaris (<a href="comments/feed">RSS</a>).</p>
 			<p class="logogencat">Logo <acronym title="Generalitat de Catalunya"><a href="http://www.gencat.cat/educacio">GENCAT<span></span></a></acronym></p>
 		</div>
 	</div> <!-- end of sidebar -->
