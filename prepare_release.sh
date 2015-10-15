7cb84bdf92464ebf94f515e0c81456f1951e3f49
Pau Ferrer Ocaña <crazyserver@gmail.com>
2015-10-15 14:33:00 +0200

    Add prepare release script

---
+++ prepare_release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ] ; then
+    echo "Script per comprimir la release"
+    echo "Forma d'ús: prepare_release.sh <version>"
+    echo "Exemple invocació: ./prepare_release.sh 14.09.30"
+    exit 0
+fi
+
+version=$1
+app=xtecblocs
+filename=$app"_v"$version
+
+cd /tmp
+git clone https://github.com/projectestac/$app.git
+cd $app
+git submodule update --recursive --init
+
+find . -name '\.git*' -exec rm -rf {} \;
+
+echo "Comprimint $filename.tar.gz"
+tar cfzp ../$filename.tar.gz *
+
+echo "Comprimint $filename.zip"
+zip -r -q ../$filename.zip .
+echo "Fet"
